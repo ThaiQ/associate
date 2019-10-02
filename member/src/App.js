999b3ed5b92d183ba9e48dccca2714e45864f1ee
Nanar Boursalian <nanarboursalian@MacBook-Pro.local>
2019-10-02 11:34:43 -0700

    e PleasL enter the commit message for your changes. Lines starting

--- member/src/App.js
+++ member/src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-
-      <div className="App-header">
-      <header id="head">
-      <h1>SCE Officers</h1>
-      <p>Officers and Associates of SCE</p>
-      <body> body </body>
-      </header>
-      </div>
-
 
       <div className="App-header">
       <header id="head">
