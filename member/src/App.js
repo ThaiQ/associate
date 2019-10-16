4770c8655c0cb5d6636096323eca0449350a9902
Seema Vora <seemavora@Seemas-MacBook-Air.local>
2019-10-16 15:30:07 -0700

    e Please enter the commit message for your changes. Lines starting

--- member/src/App.js
+++ member/src/App.js
@@ -8,9 +8,24 @@ function App() {
       <div className="App-header">
       <header id="head">
       <h1>SCE Officers</h1>
-      <p>Officers and Associates of SCE</p>
-      <body> body </body>
+      <p>Officers and Associates </p>
+      <body></body>
       </header>
+      </div>
+
+      <div className="tab">
+        <button class="tablinks" onclick="openCity(event, '2018-2019')">2018-2019</button>
+        <button class="tablinks" onclick="openCity(event, '2019-2020')">2019-2020</button>
+      </div>
+
+      <div id="year1" class="tabcontent">
+        <h3>2018-2019</h3>
+        <p>Paris is the capital of France.</p>
+      </div>
+
+      <div id="year2" class="tabcontent">
+        <h3>2019-2020</h3>
+        <p>Tokyo is the capital of Japan.</p>
       </div>
 
     </div>
