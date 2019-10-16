cd28c93859db9269360355fa7d129380a37ee287
Nanar Boursalian <nanarboursalian@MacBook-Pro.local>
2019-10-16 10:13:25 -0700

    e Please enter the commit message for your changes. Lines starting

--- member/src/App.js
+++ member/src/App.js
@@ -1,5 +1,7 @@
+
 import React from 'react';
 import './App.css';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 
 function App() {
   return (
@@ -12,6 +14,17 @@ function App() {
       <body> body </body>
       </header>
       </div>
+
+      <ButtonToolbar>
+        <Button variant="outline-primary">Primary</Button>
+        <Button variant="outline-secondary">Secondary</Button>
+        <Button variant="outline-success">Success</Button>
+        <Button variant="outline-warning">Warning</Button>
+        <Button variant="outline-danger">Danger</Button>
+        <Button variant="outline-info">Info</Button>
+        <Button variant="outline-light">Light</Button>
+        <Button variant="outline-dark">Dark</Button>
+      </ButtonToolbar>
 
     </div>
 
