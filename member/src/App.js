6474a613ffcd862705d7a0a8cd7c942fb35f9376
Seema Vora <seemavora@Seemas-MacBook-Air.local>
2019-10-16 15:49:23 -0700

    kjrg

--- member/src/App.js
+++ member/src/App.js
@@ -11,38 +11,14 @@ function App() {
       <header id="head">
       <h1>SCE Officers</h1>
       <p>Officers and Associates </p>
-      <body></body>
-      </header>
-      </div>
-
-<<<<<<< HEAD
+      <body>
       <div className="tab">
         <button class="tablinks" onclick="openCity(event, '2018-2019')">2018-2019</button>
         <button class="tablinks" onclick="openCity(event, '2019-2020')">2019-2020</button>
       </div>
-
-      <div id="year1" class="tabcontent">
-        <h3>2018-2019</h3>
-        <p>Paris is the capital of France.</p>
+      </body>
+      </header>
       </div>
-
-      <div id="year2" class="tabcontent">
-        <h3>2019-2020</h3>
-        <p>Tokyo is the capital of Japan.</p>
-      </div>
-=======
-      <ButtonToolbar>
-        <Button variant="outline-primary">Primary</Button>
-        <Button variant="outline-secondary">Secondary</Button>
-        <Button variant="outline-success">Success</Button>
-        <Button variant="outline-warning">Warning</Button>
-        <Button variant="outline-danger">Danger</Button>
-        <Button variant="outline-info">Info</Button>
-        <Button variant="outline-light">Light</Button>
-        <Button variant="outline-dark">Dark</Button>
-      </ButtonToolbar>
->>>>>>> cd28c93859db9269360355fa7d129380a37ee287
-
     </div>
 
   );
