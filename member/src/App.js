be41eaeaa2cd622e2982d41f60cd09763d3c0fe1
Nanar Boursalian <nanarboursalian@MacBook-Pro.local>
2019-10-16 15:31:57 -0700

    e Please enter the commit message for your changes. Lines starting

--- member/src/App.js
+++ member/src/App.js
@@ -18,12 +18,6 @@ function App() {
       <ButtonToolbar>
         <Button variant="outline-primary">Primary</Button>
         <Button variant="outline-secondary">Secondary</Button>
-        <Button variant="outline-success">Success</Button>
-        <Button variant="outline-warning">Warning</Button>
-        <Button variant="outline-danger">Danger</Button>
-        <Button variant="outline-info">Info</Button>
-        <Button variant="outline-light">Light</Button>
-        <Button variant="outline-dark">Dark</Button>
       </ButtonToolbar>
 
     </div>
