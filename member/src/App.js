a0402ec3fdb5c5bc1d080a47ab20b1ba0a2a6042
Nanar Boursalian <nanarboursalian@MacBook-Pro.local>
2019-10-16 15:58:30 -0700

    db

--- member/src/App.js
+++ member/src/App.js
@@ -9,7 +9,24 @@ function App() {
 
       <div className="App-header">
       <header id="head">
-      <h1>SCE Officers</h1>
+      <h1>
+        <div className="tab">
+          <button class="tablinks" onclick="openCity(event, '2018-2019')">2018-2019</button>
+          <button class="tablinks" onclick="openCity(event, '2019-2020')">2019-2020</button>
+        </div>
+
+        <div id="year1" class="tabcontent">
+          <h3>2018-2019</h3>
+          <p>Paris is the capital of France.</p>
+        </div>
+
+        <div id="year2" class="tabcontent">
+          <h3>2019-2020</h3>
+          <p>Tokyo is the capital of Japan.</p>
+        </div>
+      </h1>
+
+      <h2> SCE Officers </h2>
       <p>Officers and Associates </p>
       <body>
         <table class="table">
@@ -49,30 +66,6 @@ function App() {
       </body>
       </header>
       </div>
-
-      <div className="tab">
-        <button class="tablinks" onclick="openCity(event, '2018-2019')">2018-2019</button>
-        <button class="tablinks" onclick="openCity(event, '2019-2020')">2019-2020</button>
-      </div>
-
-      <div id="year1" class="tabcontent">
-        <h3>2018-2019</h3>
-        <p>Paris is the capital of France.</p>
-      </div>
-
-      <div id="year2" class="tabcontent">
-        <h3>2019-2020</h3>
-        <p>Tokyo is the capital of Japan.</p>
-      </div>
-
-
-
-
-
-      <ButtonToolbar>
-        <Button variant="outline-primary">Primary</Button>
-        <Button variant="outline-secondary">Secondary</Button>
-      </ButtonToolbar>
 
     </div>
 
